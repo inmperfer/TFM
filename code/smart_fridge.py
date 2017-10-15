429ebca7e39da445b337fae23925082ce1c1eb02
Inmaculada Perea Fernandez <inma.perea.fdz@gmail.com>
2017-10-15 22:41:59 +0200

    devuelve receta y método de cocinado segun la foto reconocida. Refactorización de codigo, y nuevos metodos API food2fork

--- code/smart_fridge.py
+++ code/smart_fridge.py
@@ -24,8 +24,10 @@ AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 
 # Define our connection string
-db_string_connection = "host='localhost' dbname='smartfridge' user='postgres' password='postgres'"
+DB_STRING_CONNECTION = "host='localhost' dbname='smartfridge' user='postgres' password='postgres'"
 
+# 1 second delay between reading from firehose
+READ_WEBSOCKET_DELAY = 1
 
 VISUAL_RECOGNITION_VERSION = '2017-10-15'
 VISUAL_RECOGNITION_URL = 'https://gateway-a.watsonplatform.net/visual-recognition/api'
@@ -35,15 +37,16 @@ class SmartFridge():
     def __init__(self):
         #  instantiate Slack
         self.slack_client = SlackClient(SLACK_BOT_TOKEN)
-        self.food2fork_key = FOOD2FORK_KEY
-        self.database_cursor = None
+
+        #  instantiate Watson recognition service
         self.visual_recognition = VisualRecognitionV3(version=VISUAL_RECOGNITION_VERSION,
                                                       url=VISUAL_RECOGNITION_URL,
                                                       api_key=VISUAL_RECOGNITION_KEY)
+        self.database_cursor = None
 
 
-    def getdbinformation(self):
-        # execute our Query
+    def get_db_information(self):
+        # execute Query
         self.database_cursor.execute("SELECT name FROM products")
 
         # retrieve the records from the database
@@ -67,7 +70,7 @@ class SmartFridge():
         if command.startswith('do'):
             response = "Sure...write some more code then I can do that!"
         elif command.startswith('db'):
-            response=self.getdbinformation()
+            response=self.get_db_information()
         elif command.startswith('photo'):
             with open('./download/food.jpg', 'rb') as image_file:
                 vr_response = smartfridge.visual_recognition.classify(images_file=image_file,
@@ -80,7 +83,9 @@ class SmartFridge():
                         if classifier['classes'] and len(classifier['classes'])>0:
                             food=classifier['classes'][0]['class']
                             score=classifier['classes'][0]['score']
-                            response='This is... {0} (score: {1})'.format(food, score)
+                            response='Uhm... This is {0} (score: {1})'.format(food, score)
+                            response=response + '\n' + smartfridge.get_ingredients(smartfridge.get_recipe_id(food))
+                            
 
         # Provide response
         self.slack_client.api_call("chat.postMessage",
@@ -111,31 +116,23 @@ class SmartFridge():
         return None, None
 
 
-
     def database_connection(self):
         # print the connection string we will use to connect
-        print('Connecting to database ... {}'.format(db_string_connection))
+        print('Connecting to database ... {}'.format(DB_STRING_CONNECTION))
 
         # get a connection, if a connect cannot be made an exception will be raised here
-        db_conn = psycopg2.connect(db_string_connection)
+        db_conn = psycopg2.connect(DB_STRING_CONNECTION)
 
         # conn.cursor will return a cursor object, you can use this cursor to perform queries
         self.database_cursor = db_conn.cursor()
 
     def _urlHelper(self, endpoint, **kwargs):
-        data = {'key': self.food2fork_key}
+        data = {'key': FOOD2FORK_KEY}
 
         for key, value in kwargs.items():
             data[key] = value
 
         return endpoint + '?' + urllib.parse.urlencode(data)
-
-
-    def recipes_by_ingredients(self, query):
-        endpoint = 'http://food2fork.com/api/search'
-        url = self._urlHelper(endpoint, q=query)
-        print(url)
-        return requests.get(url).json()
 
 
     def download_file(self, url, local_filename, basedir):
@@ -156,18 +153,55 @@ class SmartFridge():
 
         return True
 
+    #######   FOOD2FORK    #######
+    def search_recipes(self, query):
+        endpoint = 'http://food2fork.com/api/search'
+        url = self._urlHelper(endpoint, q=query)
+        return requests.get(url).json()
+
+    def get_recipe_from_id(self, recipeId):
+        endpoint = 'http://food2fork.com/api/get'
+        try:
+            url = self._urlHelper(endpoint, rId=recipeId)
+            return requests.get(url).json()
+        except Exception as inst:
+            print(inst)
+            return None
+
+    def get_ingredients(self, recipeId):
+        recipe=self.get_recipe_from_id(recipeId)
+        if recipe and 'recipe' in recipe:
+            ingredients=[]
+            source = 'Source URL: {}'.format(recipe['recipe']['source_url'])
+            for ingredient in (recipe['recipe']['ingredients']):
+                ingredients.append(ingredient)
+                str_ingredients='Ingredients:' + '\n{}'.format('\n'.join(map(str, ingredients)))
+            return (str_ingredients + '\n' + source)
+
+
+    def get_recipes_from_ingredients(self, ingredients):
+        recipes=self.search_recipes(ingredients)
+        if recipes and 'recipes' in recipes:
+            for i, recipe in enumerate(recipes['recipes'][:5]):
+                print('[{0}] : {1}'.format(i+1, recipe['title']))
+
+    def get_recipe_id(self, query):
+        recipes=self.search_recipes(query)
+        if recipes and 'recipes' in recipes and len(recipes['recipes'])>0:
+            return(recipes['recipes'][0]['recipe_id'])
+        else:
+            return None
+
+
+
 if __name__ == "__main__":
     smartfridge=SmartFridge()
     smartfridge.database_connection()
 
-    ingredients='chicken, tomatoes, cheese, onion, egg'
-    recipes=smartfridge.recipes_by_ingredients(ingredients)['recipes']
-
-    for i, recipe in enumerate(recipes):
-        print('[{0}] : {1}'.format(i+1, recipe['title']))
+    ingredients = 'chicken, tomatoes, cheese, onion, egg'
+    smartfridge.get_recipes_from_ingredients(ingredients)
 
 
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if smartfridge.slack_client.rtm_connect():
         print("smartfridge connected and running!")
         while True:
