09f63a2fa4ca23593bce8ae2045978be679d0e64
Inmaculada Perea Fernandez <inma.perea.fdz@gmail.com>
2017-12-06 12:54:15 +0100

    revisado apartado de selección de plataforma de mensajeria y API de recetas

---
+++ tests/food2fork.py
@@ -0,0 +1,194 @@
+import urllib.request as urllib2
+import urllib
+import json
+from functools import partial
+
+import random
+
+ 
+
+class food2fork():
+	'''
+	Usage:
+		import food2fork
+		f2f = food2fork("My Api Key")
+		recipes = f2f.search("chicken, rice")
+		moreRecipes = f2f.search("chicken, rice", 2)
+	'''
+
+	def __init__(self, debug=True):
+
+		self.apiKey = '0503617cd3c134aa02ae9a96824bf519'
+
+		self.debugMode = debug
+
+		self.SEARCH_ENDPOINT = 'http://food2fork.com/api/search'
+		self.VIEW_ENDPOINT   = 'http://food2fork.com/api/get'
+		self.MAX_PAGESIZE = 30
+
+
+
+	def search(self, query):
+		try:
+			url = self._urlHelper(self.SEARCH_ENDPOINT, q=query)
+
+			contents = self._getUrlContents(url)
+
+			data = json.loads(contents)
+
+			if(self.debugMode):
+				print(contents)
+
+			return data
+
+		except Exception as inst:
+			if(self.debugMode):
+				print(inst)
+
+			return None
+
+	def getTopRated(self,page = 1,pageSize = 30):
+		if(pageSize > self.MAX_PAGESIZE):
+			pageSize = self.MAX_PAGESIZE
+
+		try:
+			url = self._urlHelper(self.SEARCH_ENDPOINT,page=page,count=pageSize)
+
+			contents = self._getUrlContents(url)
+			
+			data = json.loads(contents)
+
+			if(self.debugMode):
+				print(contents)
+
+			return data
+		
+		except Exception as inst:
+			if(self.debugMode):
+				print(inst)
+
+			return None
+	
+	def getTrending(self,page = 1,pageSize = 30):
+		try:
+			url = self._urlHelper(self.SEARCH_ENDPOINT,page=page,count=pageSize,sort='t')
+
+			contents = self._getUrlContents(url)
+
+			data = json.loads(contents)
+
+			if(self.debugMode):
+				print(contents)
+
+			return data
+		
+		except Exception as inst:
+			if(self.debugMode):
+				print(inst)
+
+			return None
+
+	def getRecipe(self,recipeId):
+		try:
+			url = self._urlHelper(self.VIEW_ENDPOINT,rId=recipeId)
+
+			contents = self._getUrlContents(url)
+
+			data = json.loads(contents)
+
+			return data
+
+		except Exception as inst:
+			if(self.debugMode):
+				print(inst)
+
+			return None
+
+	def _urlHelper(self,endpoint,**kwargs):
+		data = { 'key' : self.apiKey }
+
+		for key, value in kwargs.iteritems():
+			data[key] = value
+
+		if(self.debugMode):
+			print ("Url:", endpoint + '?' + urllib.urlencode(data))
+
+		return endpoint + '?' + urllib.urlencode(data)
+
+	def _getUrlContents(self,url):
+		try:
+			response = urllib2.urlopen(url)
+
+			contents = response.read()
+
+			return contents
+
+		except Exception as inst:
+			if(self.debugMode):
+				print(inst)
+
+			return None
+		
+#######################################################################
+	#method added 	
+	#the if else doesn't work:(
+	def getRandomRecipe(self, query = None):
+		if query == None:
+			try:
+				
+				if self.getTrending() is not None:
+					recipes = self.getTrending()['recipes']	
+					random.shuffle(recipes)
+					return recipes[0]
+				else:
+					if self.getTopRated() is not None:
+						recipes = self.getTrending()['recipes']	
+						random.shuffle(recipes)
+						return recipes[0]					
+					
+					
+			except Exception as inst:
+				if(self.debugMode):
+					print(inst)
+	
+				return None	
+		else:
+			try:
+				f2f = self.search(query) 
+				if f2f is not None:
+					 recipes = f2f['recipes']
+					 random.shuffle(recipes)
+					 return recipes[0]
+				 
+			except Exception as inst:
+				if(self.debugMode):
+					print(inst)
+				return None
+				
+	
+	def getRandomRecipeID(self, query = None):
+		if query == None:
+			try:
+				
+				recipe = self.getRandomRecipe()
+				return recipe['recipe_id']
+			except Exception as inst:
+				if(self.debugMode):
+					print(inst)
+	
+				return None	
+		else:
+			try:
+				recipe = self.getRandomRecipe(query)
+				return recipe['recipe_id']
+			except Exception as inst:
+				if(self.debugMode):
+					print(inst)
+				return None
+			
+		
+	
+		
+		
+	
+	
